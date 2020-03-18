7c3a0ecc328f3a4b9f25be8d5264abf5089d7e71
mastaryan <sizemore.ryan@gmail.com>
2020-03-18 18:28:30 -0400

    MVP

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,35 +10,34 @@ module.exports = {
 }
 
 function find() {
-    return db("scheme");
+    return db("schemes");
 }
 
 function findById(id) {
-    return db("scheme")
+    return db("schemes")
         .where({ id })
-        .then(scheme => {scheme ? scheme : null})
-        .first();
+        .first()
 }
 
 function findSteps(id) {
-    return db("scheme as sch")
+    return db("schemes as sch")
         .join("steps as s", "s.scheme_Id", "sch.id" )
         .select("sch.id", "sch.scheme_name", "s.step_number", "s.instructions")
         .where("sch.id", id);
 }
 
 function add(scheme) {
-    return db("scheme").insert(scheme, id);   
+    return db("schemes").insert(scheme);   
 }
 
 function update(changes, id) {
-    return db("scheme")
+    return db("schemes")
         .where({ id })
         .update(changes);
 }
 
 function remove(id) {
-    return db("scheme")
+    return db("schemes")
         .where({ id })
         .del();
 }
